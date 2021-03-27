98ceb07912aedc9ebfb49f0faecd2bc5f54f8481
wty-andrew <wty.andrew@gmail.com>
2021-03-27 06:14:56 +0000

    Deploying to gh-pages from @ wty-andrew/gatsby-mdx-deck-template@95f91ba19bac52c7dacc95fe57afda2d2b295fa1 🚀

---
+++ component---src-pages-index-js-99d17e56d9284a4689c9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{RXBc:function(c,n,t){"use strict";t.r(n);var e=t("q1tI"),l=t.n(e),u=t("Wbzz"),b=t("qKvR");n.default=function(){return Object(b.c)(l.a.Fragment,null,Object(b.c)("h1",null,"Gatsby MDX-Deck Template"),Object(b.c)("ul",null,Object(b.c)("li",null,Object(b.c)(u.Link,{to:"/markdown"},"Markdown")),Object(b.c)("li",null,Object(b.c)(u.Link,{to:"/misc"},"MISC"))))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-99d17e56d9284a4689c9.js.map
